922be08bb30714d2911a36b7373ff1a04ead2086
liceth1006 <olmos.1006@gmail.com>
2024-04-17 22:36:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+<button className='btn btn-outline-danger '>
+  Button
+  <i className="fas fa-coffee"></i>
+</button>
+    </div>
+  );
+}
+
+export default App;
