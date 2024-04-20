092ad896f84a36cd1e0359f7cad104c7e711843c
liceth1006 <olmos.1006@gmail.com>
2024-04-19 19:09:38 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  
   return (
     <>
-      <ul className="nav nav-tabs  mb-0 " >
+      <ul className="nav  nav-underline  mb-0  " >
         {["Basico", "Avanzado", "Historial"].map((item, index) => (
           <li className="nav-item ms-2  " key={index}>
             <Link
-              className="nav-link fs-3"
+               className="nav-link fs-3 "
               aria-current="true"
               to={item === "Basico" ? "/" : `/${item.toLowerCase()}`}
             >
